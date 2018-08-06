9fc45ecd90ca84642f3144cb334869bf9b4d35e7
Richard Ramos <info@richardramos.me>
2018-08-06 08:42:08 -0400

    Added card for the form

--- app/js/components/Create.js
+++ app/js/components/Create.js
@@ -1,11 +1,12 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component} from 'react';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import EmbarkJS from 'Embark/EmbarkJS';
 import PropTypes from 'prop-types';
 import ReportManager from 'Embark/contracts/ReportManager';
 import Spinner from 'react-spinkit';
 import TextField from '@material-ui/core/TextField';
-
 import web3 from 'Embark/web3';
 import {withStyles} from '@material-ui/core/styles';
 
@@ -68,21 +69,23 @@ class Create extends Component{
     const {classes} = this.props;
 
     return (
-      <Fragment>
-        <TextField
-          id="description"
-          label="Descripci&oacute;n"
-          multiline
-          rowsMax="20"
-          fullWidth
-          value={this.state.text}
-          onChange={this.handleChange('text')}
-          className={classes.textField}
-          margin="normal" />
-        {
-          this.state.isSubmitting ? <Spinner name="circle" color="blue"/> : <Button variant="contained" color="primary" onClick={this.handleClick}>Publicar</Button>
-        }
-      </Fragment>
+      <Card>
+        <CardContent>
+          <TextField
+            id="description"
+            label="Descripci&oacute;n"
+            multiline
+            rowsMax="20"
+            fullWidth
+            value={this.state.text}
+            onChange={this.handleChange('text')}
+            className={classes.textField}
+            margin="normal" />
+          {
+            this.state.isSubmitting ? <Spinner name="circle" color="blue"/> : <Button variant="contained" color="primary" onClick={this.handleClick}>Publicar</Button>
+          }
+        </CardContent>
+      </Card>
     );
   }
 }
