415eb0cb2cf9bf06c34995038d6ea67d1be55475
Richard Ramos <info@richardramos.me>
2018-08-09 20:18:34 -0400

    Added title to form

--- app/js/components/Create.js
+++ app/js/components/Create.js
@@ -1,11 +1,11 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import EmbarkJS from 'Embark/EmbarkJS';
+import LinearProgress from '@material-ui/core/LinearProgress';
 import PropTypes from 'prop-types';
 import ReportManager from 'Embark/contracts/ReportManager';
-import Spinner from 'react-spinkit';
 import TextField from '@material-ui/core/TextField';
 import web3 from 'Embark/web3';
 import {withStyles} from '@material-ui/core/styles';
@@ -23,20 +23,35 @@ class Create extends Component{
     super(props);
     this.state = {
       'text': '',
-      'isSubmitting': false
+      'title': '',
+      'isSubmitting': false,
+      'error': ''
     };
   }
 
   handleClick = event => {
     event.preventDefault();
 
-    this.setState({isSubmitting: true});
+    if(this.state.text.trim() == ''){
+      this.setState({'error': 'Campo Requerido'});
+      return;
+    }
+
+    this.setState({
+      isSubmitting: true, 
+      'error': ''
+    });
 
     const {create} = ReportManager.methods;
     
     let toSend;
 
-    EmbarkJS.Storage.saveText(this.state.text)
+    const textToSave = {
+      'title': this.state.title,
+      'content': this.state.text
+    };
+
+    EmbarkJS.Storage.saveText(JSON.stringify(textToSave))
     .then(ipfsHash => {
       toSend = create(web3.utils.toHex(ipfsHash));
       return toSend.estimateGas();
@@ -67,25 +82,42 @@ class Create extends Component{
 
   render(){
     const {classes} = this.props;
+    const {error, text, title, isSubmitting} = this.state;
 
-    return (
+    return (<Fragment>
       <Card>
         <CardContent>
           <TextField
-            id="description"
-            label="Descripci&oacute;n"
+            id="title"
+            label="T&iacute;tulo"
+            error={error != ""}
             multiline
             rowsMax="20"
             fullWidth
-            value={this.state.text}
+            value={title}
+            helperText={error}
+            onChange={this.handleChange('title')}
+            className={classes.textField}
+            margin="normal" />
+          <TextField
+            id="description"
+            label="Descripci&oacute;n"
+            error={error != ""}
+            multiline
+            rowsMax="20"
+            fullWidth
+            value={text}
+            helperText={error}
             onChange={this.handleChange('text')}
             className={classes.textField}
             margin="normal" />
           {
-            this.state.isSubmitting ? <Spinner name="circle" color="blue"/> : <Button variant="contained" color="primary" onClick={this.handleClick}>Publicar</Button>
+            <Button variant="contained" color="primary" onClick={this.handleClick} disabled={isSubmitting }>Publicar</Button>
           }
         </CardContent>
       </Card>
+      { this.state.isSubmitting && <LinearProgress /> }
+      </Fragment>
     );
   }
 }
