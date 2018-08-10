3cda63d1fdd9995fec890df7cce4a4516d9d5b56
Richard Ramos <info@richardramos.me>
2018-08-09 21:05:27 -0400

    Tutorial pt 2

--- contracts/ReportManager.sol
+++ contracts/ReportManager.sol
@@ -33,52 +33,24 @@ contract ReportManager {
     function numReports()
         public
         view
-        returns(uint)
-    {
-        return reports.length;
-    }
+        returns(uint){
+            return 1;
+        }
 
     // @notice Create report
     // @param _description IPFS hash of the content of the report
     function create(bytes _description)
-        public
-    {
-        uint reportId = reports.length++;
-        reports[reportId] = Report({
-            /* solium-disable-next-line */
-            creationDate: block.timestamp,
-            description: _description,
-            owner: msg.sender,
-            upvotes: 0,
-            downvotes: 0
-        });
+        public {
 
-        emit NewReport(reportId, msg.sender, _description);
-    }
+        }
 
     // @notice Vote on a report
     // @param _reportId Id of the report to up/downvote
     // @param _vote Vote selection: 0 -> none, 1 -> upvote, 2 -> downvote
     function vote(uint _reportId, uint8 _vote)
-        public
-    {
-        Report storage r = reports[_reportId];
+        public {
 
-        require(r.creationDate != 0, "Report does not exist");
-        require(r.voters[msg.sender] == Ballot.NONE, "You already voted on this report");
-
-        Ballot b = Ballot(_vote);
-
-        if (b == Ballot.UPVOTE) {
-            r.upvotes++;
-        } else {
-            r.downvotes++;
         }
-
-        r.voters[msg.sender] = b;
-
-        emit Vote(_reportId, msg.sender, _vote);
-    }
 
     // @notice Determine if the sender can vote on a report
     // @param _reportId Id of the report
@@ -86,14 +58,9 @@ contract ReportManager {
     function canVote(uint _reportId)
         public
         view
-        returns (bool)
-    {
-        if(_reportId > reports.length - 1) return false;
-        
-        Report storage r = reports[_reportId];
-        
-        return (r.creationDate != 0 && r.voters[msg.sender] == Ballot.NONE);
-    }
+        returns (bool){
+            return false;
+        }
 
     // @notice Obtain vote for specific report
     // @param _reportId Id of the report
@@ -101,10 +68,8 @@ contract ReportManager {
     function getVote(uint _reportId)
         public
         view
-        returns (uint8)
-    {
-        Report storage r = reports[_reportId];
-        return uint8(r.voters[msg.sender]);
-    }
+        returns (uint8){
+            return 1;
+        }
 
 }
