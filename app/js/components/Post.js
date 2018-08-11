71986e6a6240409c9f1daa4c0a2227fb2afa1b72
Richard Ramos <info@richardramos.me>
2018-08-11 08:16:13 -0400

    Removed code from post

--- app/js/components/Post.js
+++ app/js/components/Post.js
@@ -1,18 +1,15 @@
 import {Card, CardActions, CardContent, CardHeader} from '@material-ui/core';
 import React, {Component} from 'react';
 import Blockies from 'react-blockies';
-import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import DownvoteIcon from '@material-ui/icons/ExpandMore';
-import EmbarkJS from 'Embark/EmbarkJS';
 import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import PropTypes from 'prop-types';
-import ReportManager from 'Embark/contracts/ReportManager';
 import Typography from '@material-ui/core/Typography';
 import UpvoteIcon from '@material-ui/icons/ExpandLess';
 import dateformat from 'dateformat';
 import markdownJS from "markdown";
-import web3 from 'Embark/web3';
 import {withStyles} from '@material-ui/core/styles';
 
 const markdown = markdownJS.markdown;
@@ -23,11 +20,17 @@ const styles = theme => ({
       display: 'flex'
     },
     card: {
-      margin: theme.spacing.unit * 3
+      margin: theme.spacing.unit * 2,
+      marginTop: theme.spacing.unit * 4,
+      position: 'relative'
     },
     title: {
         borderBottom: '1px solid #ccc',
         color: '#666'
+    },
+    spinner: {
+        position: 'absolute',
+        right: theme.spacing.unit * 3
     }
 });  
 
@@ -37,87 +40,58 @@ const ballot = {
     DOWNVOTE: 2
 };
 
-class Report extends Component {
+class Post extends Component {
+
     constructor(props){
         super(props);
+
         this.state = {
             title: '',
             content: '',
             isSubmitting: false,
             canVote: true,
-            vote: 0,
             upvotes: props.upvotes,
             downvotes: props.downvotes
         };
     }
 
     componentDidMount(){
-        EmbarkJS.onReady(() => {
-            this._loadAttributes();
-        });
+        this._loadAttributes();
     }
 
     _loadAttributes = () => {
-        const ipfsHash = web3.utils.toAscii(this.props.description);
-        EmbarkJS.Storage.get(ipfsHash)
-        .then(ipfsText => {
-            const jsonContent = JSON.parse(ipfsText);
-            const title = jsonContent.title;
-            const content = jsonContent.content;
-            this.setState({title, content});
-        });
 
-        const {canVote, getVote} = ReportManager.methods;
-
-        canVote(this.props.id).call().then((canVote) => {
-            this.setState({canVote});
-        });
-
-        getVote(this.props.id).call().then((vote) => {
-            this.setState({vote});
+        this.setState({
+            title: "Tres leyes de la robótica",
+            content: "Un robot no hará daño a un ser humano o, por inacción, permitir que un ser humano sufra daño.\nUn robot debe cumplir las órdenes dadas por los seres humanos, a excepción de aquellas que entrasen en conflicto con la primera ley.\nUn robot debe proteger su propia existencia en la medida en que esta protección no entre en conflicto con la primera o con la segunda ley",
+            canVote: true
         });
     }
 
     _vote = choice => event => {
         event.preventDefault();
-
         this.setState({isSubmitting: true});
 
-        const {vote} = ReportManager.methods;
-        let toSend = vote(this.props.id, choice);
-        
-        toSend.estimateGas()
-        .then(estimatedGas => {
-            return toSend.send({gas: estimatedGas + 1000});
-        })
-        .then(receipt => {
-            this.setState({
-                canVote: false,
-                upvotes: this.state.upvotes + (choice == ballot.UPVOTE ? 1 : 0),
-                downvotes: this.state.downvotes + (choice == ballot.DOWNVOTE ? 1 : 0)
-            });
-
-            console.log(receipt);
-        })
-        .catch((err) => {
-            console.error(err);
-        })
-        .finally(() => {
-            this.setState({isSubmitting: false});
+        this.setState({
+            canVote: false,
+            upvotes: this.state.upvotes + (choice == ballot.UPVOTE ? 1 : 0),
+            downvotes: this.state.downvotes + (choice == ballot.DOWNVOTE ? 1 : 0)
         });
+
+        this.setState({isSubmitting: false});
     }
 
     render(){
-        const {title, content, upvotes, downvotes} = this.state;
+        const {title, content, upvotes, downvotes, isSubmitting, canVote} = this.state;
         const {creationDate, classes, owner} = this.props;
-        const disabled = this.state.isSubmitting || !this.state.canVote;
-        const formattedDate = dateformat(new Date(creationDate * 1000), "dddd, mmmm dS, yyyy, h:MM:ss TT");
+        const disabled = isSubmitting || !canVote;
+        const formattedDate = dateformat(new Date(creationDate * 1000), "yyyy-mm-dd HH:MM:ss");
         const mdText = markdown.toHTML(content);
 
         return <Card className={classes.card}>
             <CardHeader title={owner} subheader={formattedDate}
                 avatar={
-                    <Blockies seed={owner} size={8} scale={6} />
+                    <Blockies seed={owner} size={7} scale={5} />
                 }
                 action={
                 <IconButton>
@@ -131,20 +105,22 @@ class Report extends Component {
                 <Typography component="div" dangerouslySetInnerHTML={{__html: mdText}} />
             </CardContent>
             <CardActions disableActionSpacing>
-            <IconButton className={classes.actions} disabled={disabled} onClick={this._vote(ballot.UPVOTE)}>
-              <UpvoteIcon />
-              {upvotes}
-            </IconButton>
-            <IconButton className={classes.actions} disabled={disabled} onClick={this._vote(ballot.DOWNVOTE)}>
-              <DownvoteIcon />
-              {downvotes}
-            </IconButton>
+                <IconButton className={classes.actions} disabled={disabled} onClick={this._vote(ballot.UPVOTE)}>
+                <UpvoteIcon />
+                {upvotes}
+                </IconButton>
+                <IconButton className={classes.actions} disabled={disabled} onClick={this._vote(ballot.DOWNVOTE)}>
+                <DownvoteIcon />
+                {downvotes}
+                </IconButton>
+                { isSubmitting && <CircularProgress size={14} className={classes.spinner} /> }
           </CardActions>
         </Card>;
     }
+    
 }
 
-Report.propTypes = {
+Post.propTypes = {
     upvotes: PropTypes.number.isRequired,
     downvotes: PropTypes.number.isRequired,
     classes: PropTypes.object.isRequired,
@@ -155,4 +131,4 @@ Report.propTypes = {
   };
   
 
-export default withStyles(styles)(Report);
+export default withStyles(styles)(Post);
